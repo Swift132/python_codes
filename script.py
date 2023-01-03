6a04adc41e5f7f0a9dbf67a27e28bb77b23c38f4
Swift132 <diogopacheco132@gmail.com>
2023-01-03 22:16:54 -0100

    Pequeno TPC
    Para elaborar o trabalho, fiz uma um dicionário com 10 Pessoas em que está incluído dentro da mesma diversas chaves como nome idade e gênero.
    Para mostrar o conteúdo fiz um ciclo for que mostra os valores que se encontram dentro das chaves, conforme o pedido.

---
+++ script.py
@@ -0,0 +1,27 @@
+#Dicionário de Pessoas com 10 Entradas.
+#Diogo Pacheco
+
+
+pessoas = {
+    "Pessoa 1": {"nome": "João", "idade": 30, "genero": "Masculino"},
+    "Pessoa 2": {"nome": "Maria", "idade": 25, "genero": "Feminino"},
+    "Pessoa 3": {"nome": "Carlos", "idade": 35, "genero": "Masculino"},
+    "Pessoa 4": {"nome": "Ana", "idade": 22, "genero": "Feminino"},
+    "Pessoa 5": {"nome": "Luiz", "idade": 40, "genero": "Masculino"},
+    "Pessoa 6": {"nome": "Carla", "idade": 29, "genero": "Feminino"},
+    "Pessoa 7": {"nome": "Pedro", "idade": 32, "genero": "Masculino"},
+    "Pessoa 8": {"nome": "Julia", "idade": 24, "genero": "Feminino"},
+    "Pessoa 9": {"nome": "Mário", "idade": 28, "genero": "Masculino"},
+    "Pessoa 10": {"nome": "Sandra", "idade": 33, "genero": "Feminino"}
+}
+
+for chave, valor in pessoas.items():
+    print(f'Chave: {chave}')
+    print(f'Nome: {valor["nome"]}')
+    print(f'Idade: {valor["idade"]}')
+    if valor["genero"] == "Masculino":
+        print("Masculino")
+    elif valor["genero"] == "Feminino":
+        print("Feminino")
+    else:
+        print("Outro")
